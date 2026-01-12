63f91698024f467f4f4682850730c26b25a21eb8
Huseyin Yilmaz <huseyin.yilmaz@cat.com>
2026-01-12 18:38:28 +0000

    update dap config

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -1,37 +1,35 @@
-
 local setup_mason_lspconfig = function()
-  -- local mason = require("mason")
-  require("lspconfig") -- Require lspconfig here
-  local mason_lspconfig = require("mason-lspconfig")
-  local handlers = require("plugins.lsp.handlers")
+	-- local mason = require("mason")
+	require("lspconfig") -- Require lspconfig here
+	local mason_lspconfig = require("mason-lspconfig")
+	local handlers = require("plugins.lsp.handlers")
 
-  mason_lspconfig.setup({
-    ensure_installed = {gopls, pyright, jsonls, black},
-    handlers = {
-      handlers.default,
-      pyright = handlers.pyright,
-      jsonls = handlers.jsonls,
-      lua_ls = handlers.lua_ls,
-      gopls = handlers.gopls,
-      },
-  })
-
+	mason_lspconfig.setup({
+		ensure_installed = { "gopls", "pyright", "jsonls", "rust_analyzer", "lua_ls", "yamlls", "golangci_lint_ls" },
+		handlers = {
+			handlers.default,
+			pyright = handlers.pyright,
+			jsonls = handlers.jsonls,
+			lua_ls = handlers.lua_ls,
+			gopls = handlers.gopls,
+		},
+	})
 end
 
 return {
-  {"neovim/nvim-lspconfig"},
+	{ "neovim/nvim-lspconfig" },
 
-  {
-    "williamboman/mason.nvim",
-    opts = {
-      ui = {
-        border = "rounded",
-      },
-    },
-  },
+	{
+		"williamboman/mason.nvim",
+		opts = {
+			ui = {
+				border = "rounded",
+			},
+		},
+	},
 
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = setup_mason_lspconfig,
-  },
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = setup_mason_lspconfig,
+	},
 }
--- lua/plugins/nvimdap.lua
+++ lua/plugins/nvimdap.lua
@@ -1,57 +1,51 @@
-return {
+local setup_dap_python = function(dap)
+  local host = "127.0.0.1"
+	local port_python = 5678
 
+	dap.adapters.debugpy = {
+		type = "server",
+		host = host,
+		port = port_python,
+	}
+
+	dap.configurations.python = {
+		{
+			name = "Attach to 127.0.0.1:5678", -- configuration for attaching to a running process
+			type = "debugpy",
+			request = "attach",
+			mode = "remote",
+		},
+	}
+end
+
+local setup_dap_go = function(dap)
+  local host = "127.0.0.1"
+  local port_go = 38697
+
+	dap.adapters.delve = {
+		type = "server",
+		host = host,
+		port = port_go,
+	}
+	-- Define configurations for Go debugging
+	dap.configurations.go = {
+		{
+			name = "Attach to 127.0.0.1:38697", -- configuration for attaching to a running process
+			type = "delve",
+			request = "attach",
+			mode = "remote",
+		},
+	}
+end
+
+return {
 	{
 		"mfussenegger/nvim-dap",
 		config = function()
 			local dap = require("dap")
-			local host = "127.0.0.1"
-			local port_go = 38697
-			local port_python = 38698
-			-- Add delve adapter to dap
-			dap.adapters.debugpy = {
-				type = "server",
-				host = host,
-				port = port_python,
-			}
 
-			dap.configurations.python = {
-				{
-					name = "Attach to 127.0.0.1:38698", -- configuration for attaching to a running process
-					type = "debugpy",
-					request = "attach",
-					mode = "remote",
-				},
-			}
-
-			dap.adapters.delve = {
-				type = "server",
-				host = host,
-				port = port_go,
-			}
-			-- Define configurations for Go debugging
-			dap.configurations.go = {
-				{
-					name = "Attach to 127.0.0.1:38697", -- configuration for attaching to a running process
-					type = "delve",
-					request = "attach",
-					mode = "remote",
-				},
-				-- {
-				--   type = "delve",
-				--   name = "Debug test", -- configuration for debugging test files
-				--   request = "attach",
-				--   mode = "test",
-				--   program = "${file}"
-				-- },
-				-- -- works with go.mod packages and sub packages
-				-- {
-				--   type = "delve",
-				--   name = "Debug test (go.mod)",
-				--   request = "attach",
-				--   mode = "test",
-				--   program = "./${relativeFileDirname}"
-				-- }
-			}
+      setup_dap_python(dap)
+      setup_dap_go(dap)
 		end,
 	},
 	{ "nvim-neotest/nvim-nio", dependencies = { "mfussenegger/nvim-dap" } },
@@ -71,3 +65,19 @@ return {
 		end,
 	},
 }
+
+-- {
+--   type = "delve",
+--   name = "Debug test", -- configuration for debugging test files
+--   request = "attach",
+--   mode = "test",
+--   program = "${file}"
+-- },
+-- -- works with go.mod packages and sub packages
+-- {
+--   type = "delve",
+--   name = "Debug test (go.mod)",
+--   request = "attach",
+--   mode = "test",
+--   program = "./${relativeFileDirname}"
+-- }
